9d9db0f03f14e099238934cb2c50716987278f32
MFega <107252430+MFega@users.noreply.github.com>
2022-10-22 03:03:41 -0700

    Add files via upload

---
+++ function.js
@@ -0,0 +1,7 @@
+const share = document.querySelector(".share");
+const link = document.querySelector(".link");
+const profile = document.querySelector(".profile")
+
+profile.addEventListener("click",()=>{
+    profile.classList.toggle("active")
+})
